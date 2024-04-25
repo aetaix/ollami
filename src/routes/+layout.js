7add5f674cb298b9fd34ce0d707e55909961c23e
Adrien <aetaix@pixelalliance.io>
2024-04-25 12:59:27 +0200

    Refactor load function in +layout.js to use fetch instead of ollama.list

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,24 +1,38 @@
-import ollama from 'ollama/browser'
+import ollama from "ollama/browser";
 import { initialModels } from "$lib/stores/models";
 
 /**
  * Load models from API and merge with initial models.
  * @type {import('./$types').LayoutServerLoad}
  */
-export async function load() {
+export async function load({fetch}) {
   // Create a deep copy of initialModels to avoid mutating the original array
   let models = JSON.parse(JSON.stringify(initialModels));
 
   try {
-    let { models: apiModels } = await ollama.list();
+    async function fetchModels() {
+      const url = "http://127.0.0.1:11434/api/tags"
+      const request = fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const response = await request;
+      console.log("response", response);
+      return response.json();
+    }
+
+    let { models: apiModels } = await fetchModels(); // ollama.list();
 
     if (apiModels.length === 0) {
       return { props: { models } };
     }
 
-    const modelMap = new Map(models.map(model => [model.image, model]));
+    const modelMap = new Map(models.map((model) => [model.image, model]));
 
-    apiModels.forEach(apiModel => {
+    apiModels.forEach((apiModel) => {
       let model = modelMap.get(apiModel.model);
 
       if (model) {
@@ -40,9 +54,8 @@ export async function load() {
 
     return { props: { models } };
   } catch (error) {
-    console.error('Failed to load models:', error);
+    console.error("Failed to load models:", error);
     // Handle errors or return a default error state
-    return { props: { models: [], error: 'Failed to load data from Ollama.' } };
+    return { props: { models: [], error: "Failed to load data from Ollama." } };
   }
 }
-
