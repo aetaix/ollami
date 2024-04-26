a5aff63b326690753475fb58a2fa55183fbd3cd0
Adrien <aetaix@pixelalliance.io>
2024-04-26 16:44:37 +0200

    Update package.json to add mode-watcher dependency

--- src/lib/components/sidebar/Dark.svelte
+++ src/lib/components/sidebar/Dark.svelte
@@ -1,46 +1,27 @@
 <script>
-  import { browser } from "$app/environment";
+  import { toggleMode, mode } from "mode-watcher";
   import Moon from "../icons/Moon.svelte";
   import Sun from "../icons/Sun.svelte";
-
-  let darkMode = false;
-
-  function handleSwitchDarkMode() {
-    darkMode = !darkMode;
-
-    localStorage.setItem("theme", darkMode ? "dark" : "light");
-
-    darkMode
-      ? document.documentElement.classList.add("dark")
-      : document.documentElement.classList.remove("dark");
-  }
-
-  if (browser) {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-      darkMode = true;
-    } else {
-      document.documentElement.classList.remove("dark");
-      darkMode = false;
-    }
-  }
 </script>
 
-
-  <button
-    on:click={handleSwitchDarkMode}
-    class="w-full outline outline-4 outline-white dark:outline-black-800 flex items-center relative rounded-lg bg-white dark:bg-black-800 "
+<button
+  on:click={toggleMode}
+  class="w-full outline outline-4 outline-white dark:outline-black-800 flex items-center relative rounded-lg bg-white dark:bg-black-800"
+>
+  <div
+    class="w-1/2 left-0 overflow-hidden bg-black-100 dark:bg-black-700 bottom-0 rounded-lg absolute top-0 transition-transform {$mode ===
+    'dark'
+      ? 'translate-x-full'
+      : ''}"
+  ></div>
+  <div
+    class="w-1/2 p-3 relative text-sm flex items-center gap-2 justify-center"
   >
-    <div
-      class="w-1/2 left-0  overflow-hidden bg-black-100 dark:bg-black-700 bottom-0 rounded-lg absolute top-0 transition-transform {darkMode
-        ? 'translate-x-full'
-        : ''}"
-    ></div>
-    <div class="w-1/2 p-3 relative text-sm flex items-center gap-2 justify-center"><Sun />Light</div>
-    <div class="w-1/2 p-3 relative text-sm flex items-center gap-2 justify-center">
-      <Moon />Dark</div>
-  </button>
+    <Sun />Light
+  </div>
+  <div
+    class="w-1/2 p-3 relative text-sm flex items-center gap-2 justify-center"
+  >
+    <Moon />Dark
+  </div>
+</button>
