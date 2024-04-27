052c196220f8987f9374e9dd6a8fa266736946ab
Adrien <aetaix@pixelalliance.io>
2024-04-27 19:21:44 +0200

    Add new SVG icons: Check and Clip

--- src/lib/components/chat/CodeHighlight.svelte
+++ src/lib/components/chat/CodeHighlight.svelte
@@ -1,24 +1,36 @@
 <script>
   import { HighlightAuto } from "svelte-highlight";
   import "svelte-highlight/styles/onedark.css";
-
+  import Clip from "../icons/Clip.svelte";
+  import Check from "../icons/Check.svelte";
 
   export let raw = "";
   export let lang = "";
   export let text = "";
-
+  let copied = false;
   function copytoclipboard() {
-    const text = raw.innerHTML.replace(/<[^>]*>?/gm, "");
-    navigator.clipboard.writeText(text);
+    const clip = text.replace(/<[^>]*>?/gm, "");
+    navigator.clipboard.writeText(clip);
+    copied = true;
+    setTimeout(() => {
+      copied = false;
+    }, 2000);
   }
-
 </script>
 
-
 <div class="bg-black-800 rounded-md font-mono">
-  <header class="bg-black-700 p-2 rounded-t-md text-xs text-black-300">
+  <header class="bg-black-700 p-2 rounded-t-md text-xs flex items-center justify-between text-black-300">
     <span>{lang}</span>
-    <button></button>
+    <button
+      on:click={copytoclipboard}
+      class="opacity-50 hover:opacity-100 text-sm cursor-pointer text-purple-600 hover:underline"
+    >
+      {#if !copied}
+        <Clip />
+      {:else}
+        <Check />
+      {/if}
+    </button>
   </header>
   <HighlightAuto code={text} />
 </div>
