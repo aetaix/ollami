49260a4a668bbdb9a8291c12f8ee414ce6b4f5a4
Adrien <aetaix@pixelalliance.io>
2024-05-16 10:58:49 +0200

    Update header height in companions and data pages

--- src/routes/companions/+page.svelte
+++ src/routes/companions/+page.svelte
@@ -12,16 +12,16 @@
             w-4/5 pl-2 transition-all absolute right-0 top-0"
   >
     <div
-      class="h-full flex flex-col overflow-y-auto relative p-6 pt-4 rounded-2xl border bg-white text-black dark:text-white border-black-200 dark:bg-black-800 dark:border-black-600 space-y-5"
+      class="h-full flex flex-col overflow-y-auto relative p-6 rounded-2xl border bg-white text-black dark:text-white border-black-200 dark:bg-black-800 dark:border-black-600 space-y-5"
     >
-      <header class="flex justify-between items-center shrink-0">
+      <header class="flex justify-between items-center shrink-0 h-14">
         <div class="flex gap-2 items-center">
             <Bot class="w-6" />
             <h1 class="items-center flex gap-3 text-xl font-semibold">
               Companions
             </h1>
         </div>
-        <CreateCompanion />
+
       </header>
       <CompanionsList />
     </div>
--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -14,7 +14,7 @@
     <div
       class="h-full flex flex-col overflow-y-auto relative p-6 rounded-2xl border bg-white text-black dark:text-white border-black-200 dark:bg-black-800 dark:border-black-600 space-y-5"
     >
-      <header class="flex justify-between items-center shrink-0">
+      <header class="flex justify-between items-center shrink-0 h-14">
 
         <h1 class="items-center flex gap-3 text-xl font-semibold">
 
