13b28a58d69d4c7f5b646af4e400f740a6221816
Adrien Etaix <adrien.etaix@hotmail.com>
2025-09-26 18:39:22 +0200

    refactor: Improve Settings dialog layout and styling for better user experience

--- src/lib/components/Settings.svelte
+++ src/lib/components/Settings.svelte
@@ -2,6 +2,7 @@
 	import { Dialog, Tabs } from 'bits-ui';
 	import Apparence from './settings-ui/Apparence.svelte';
 	import { X } from '@lucide/svelte';
+	import { fade, fly } from 'svelte/transition';
 
 	const settings = [
 		{
@@ -18,37 +19,45 @@
 </script>
 
 <Dialog.Root>
-	<Dialog.Trigger class="text-sm opacity-50 hover:opacity-100">Settings</Dialog.Trigger>
+	<Dialog.Trigger
+		class="rounded-lg p-2 text-sm text-zinc-600 transition-colors hover:bg-zinc-200 hover:text-black dark:text-zinc-400 dark:hover:bg-zinc-700 dark:hover:text-white"
+		>Settings</Dialog.Trigger
+	>
 	<Dialog.Portal>
-		<Dialog.Overlay class="fixed inset-0 z-50 bg-black/80" />
-		<Dialog.Content
-			class="fixed top-[50%] left-[50%] z-50 w-full max-w-3xl translate-x-[-50%] translate-y-[-50%] rounded-lg border border-zinc-200 bg-white dark:border-zinc-700 dark:bg-zinc-800"
-		>
-			<Tabs.Root class="grid grid-cols-6">
-				<Tabs.List class="col-span-2 flex flex-col border-r border-zinc-200 p-4">
-					<h3 class="mb-4 text-lg">Settings</h3>
+		<Dialog.Overlay>
+			<div class="fixed inset-0 z-50 bg-white/90 dark:bg-black/80"></div>
+		</Dialog.Overlay>
+		<Dialog.Content>
+			<div
+				transition:fly|global={{ y: 20, duration: 200 }}
+				class="fixed top-[50%] left-[50%] z-50 w-full max-w-3xl translate-x-[-50%] translate-y-[-50%] rounded-xl border border-zinc-200 bg-white shadow dark:border-zinc-700 dark:bg-zinc-800"
+			>
+				<Tabs.Root class="grid grid-cols-6">
+					<Tabs.List class="col-span-2 flex flex-col border-r border-zinc-200 p-4">
+						<h3 class="mb-4 text-lg">Settings</h3>
+						{#each settings as setting (setting.label)}
+							<Tabs.Trigger
+								class="rounded-lg border border-transparent p-2 text-left text-sm hover:bg-neutral-100 data-[state=active]:border-neutral-200 data-[state=active]:bg-white data-[state=active]:shadow"
+								value={setting.label}
+							>
+								{setting.label}
+							</Tabs.Trigger>
+						{/each}
+					</Tabs.List>
 					{#each settings as setting (setting.label)}
-						<Tabs.Trigger
-							class="rounded-lg border border-transparent p-2 text-left text-sm hover:bg-neutral-100 data-[state=active]:border-neutral-200 data-[state=active]:bg-white data-[state=active]:shadow"
-							value={setting.label}
-						>
-							{setting.label}
-						</Tabs.Trigger>
+						<Tabs.Content class="col-span-4" value={setting.label}>
+							<div class="p-4">
+								{#if setting.content}
+									<setting.content></setting.content>
+								{/if}
+							</div>
+						</Tabs.Content>
 					{/each}
-				</Tabs.List>
-				{#each settings as setting (setting.label)}
-					<Tabs.Content class="col-span-4" value={setting.label}>
-						<div class="p-4">
-							{#if setting.content}
-								<setting.content></setting.content>
-							{/if}
-						</div>
-					</Tabs.Content>
-				{/each}
-			</Tabs.Root>
-			<Dialog.Close class="absolute top-4 right-4 text-sm">
-				<X size={20} />
-			</Dialog.Close>
+				</Tabs.Root>
+				<Dialog.Close class="absolute top-4 right-4 text-sm">
+					<X size={20} />
+				</Dialog.Close>
+			</div>
 		</Dialog.Content>
 	</Dialog.Portal>
 </Dialog.Root>
